32ea1acd8b3ff1a93501f1364592f1d7b77e9863
samuelmarinheiro <61483919+samuelmarinheiro@users.noreply.github.com>
2026-07-11 22:58:50 +0000

    Creating new model to calculate monthly revenue

---
+++ models/monthly_revenue.sql
@@ -0,0 +1,16 @@
+with orders as (
+select *
+from {{ ref('orders') }}
+)
+
+, final as (
+
+select
+date_trunc('month', order_date) as order_month
+, sum(amount) as total_revenue
+from orders
+group by 1
+order by 1
+)
+
+select * from final
